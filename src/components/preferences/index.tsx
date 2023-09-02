ed04ce6da2e245d3f939445f8f828c7844f90c43
rohitlingarker <rohitlingerker@gmail.com>
2023-09-02 20:19:10 +0530

    livematches scores

--- src/components/preferences/index.tsx
+++ src/components/preferences/index.tsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from "@headlessui/react";
-import React, { Fragment, useState } from "react";
+import { Fragment, useState } from "react";
 
 export default function Preferences() {
   const [isOpen, setIsOpen] = useState(false);
